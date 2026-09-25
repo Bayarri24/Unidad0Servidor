72f6a5d73ab6cd1ec787f017e850fa73a825b5d6
Luis <luisbayarri12@gmail.com>
2026-09-25 09:32:25 +0200

    Subida Ejercicio2

--- src/main/java/com/mycompany/Laboral/CalculaNominas.java
+++ src/main/java/com/mycompany/Laboral/CalculaNominas.java
@@ -6,6 +6,8 @@ package com.mycompany.Laboral;
 
 import com.mycompany.Laboral.Exceptions.DatosNoCorrectosException;
 
+import java.io.*;
+
 /**
  *
  * @author usuario26
@@ -14,7 +16,7 @@ public class CalculaNominas {
 
     //Objeto nomina
     Nomina n = new Nomina();
-    
+
     private void escribe(Empleado e1, Empleado e2) {
         System.out.println("Los datos del primer empleado: ");
         e1.imprimeEmpleado();
@@ -22,9 +24,9 @@ public class CalculaNominas {
         System.out.println("Los datos del segundo empleado: ");
         e2.imprimeEmpleado();
         System.out.println("Y su sueldo es de " + n.sueldo(e2));
-        
+
     }
-    
+
     public static void main(String[] args) throws DatosNoCorrectosException {
 
         /**Creacion de objetos**/
@@ -36,10 +38,48 @@ public class CalculaNominas {
         nn.escribe(e1, e2);
         /**Incremento de los años trabajados y cambio de categoria a 9**/
         e2.incrAnyo();
-        e2.setCategoria(9);
+        e1.setCategoria(9);
 
         /**Imprimimos los empleados y su sueldo*/
         nn.escribe(e1, e2);
-        
+
+
+        //Llamamos a la función y leemos el txt
+        leerTxt("empleados.txt");
+
+    }
+
+    /**
+     * Creamos una función a la que le introducimos un String y recorremos el txt
+     *
+     */
+    public static void leerTxt(String empleado) {
+        //Con el Reader leemos linea  a linea
+        try (BufferedReader br = new BufferedReader(new FileReader(empleado))) {
+            String line;
+
+            /**
+             * Con este while le indicamos que mientras != null(va leyendo linea a linea), es decir mientras tenga linea para leer la imprime*/
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void actualizarTxt() {
+        try {
+            FileWriter fw = new FileWriter("empleados.txt");
+            fw.write("categoría: 4,años trabajados: 9, nombre: James Cosling, dni: 32000032G, sexo:´M´\n");
+            fw.close();  // must close manually
+            System.out.println("Documento actualizado");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
